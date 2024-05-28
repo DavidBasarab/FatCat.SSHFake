44106103bc86e711981f6e0da468d0e97c12c4ee
DavidBasarab <dbasarab@haivision.com>
2024-05-28 14:27:50 -0400

    basic sending working

--- src/TestingConsole/ClientTestingWorker.cs
+++ src/TestingConsole/ClientTestingWorker.cs
@@ -1,13 +1,22 @@
-﻿using FatCat.Toolkit.Console;
+﻿using FatCat.Toolkit.Communication;
+using FatCat.Toolkit.Console;
 
 namespace TestingConsole;
 
-public class ClientTestingWorker
+public class ClientTestingWorker(ITcpFactory tcpFactory)
 {
+	private const ushort ServerPort = 21456;
+
 	public async Task DoWork()
 	{
 		await Task.CompletedTask;
 
-		ConsoleLog.Write("Hello World");
+		ConsoleLog.Write($"Going to connect to TCP Server at <{ServerPort}>");
+
+		var openClient = tcpFactory.CreateOpenTcpClient();
+
+		await openClient.Connect("localhost", ServerPort);
+
+		await openClient.Send("Hello from the client");
 	}
 }
