5a34ef18f984e1d7882a695148a6351986ef5bb2
DavidBasarab <dbasarab@haivision.com>
2024-05-28 10:44:35 -0400

    basic console setup

--- src/SSHFake/Program.cs
+++ src/SSHFake/Program.cs
@@ -1,2 +1,52 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Autofac;
+using FatCat.Toolkit.Console;
+using FatCat.Toolkit.Injection;
+
+namespace FatCat.SSHFake;
+
+public static class Program
+{
+	private static IConsoleUtilities consoleUtilities;
+
+	public static string[] Args { get; set; }
+
+	public static async Task Main(params string[] args)
+	{
+		await Task.CompletedTask;
+
+		Args = args;
+
+		ConsoleLog.LogCallerInformation = true;
+
+		try
+		{
+			SystemScope.Initialize(
+				new ContainerBuilder(),
+				[typeof(Program).Assembly, typeof(ConsoleLog).Assembly],
+				ScopeOptions.SetLifetimeScope
+			);
+
+			consoleUtilities = SystemScope.Container.Resolve<IConsoleUtilities>();
+
+			var worker = SystemScope.Container.Resolve<LearningWorker>();
+
+			await worker.DoWork();
+
+			consoleUtilities.WaitForExit();
+		}
+		catch (Exception ex)
+		{
+			ConsoleLog.WriteException(ex);
+		}
+	}
+}
+
+public class LearningWorker
+{
+	public async Task DoWork()
+	{
+		await Task.CompletedTask;
+
+		ConsoleLog.Write("Hello World");
+	}
+}
